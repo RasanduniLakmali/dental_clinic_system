c0a246ec86d3f4a6deb60b51c017ee6e910b8795
RasanduniLakmali <rasandunilakmali26@gmail.com>
2026-09-05 08:47:53 +0530

    Added exception handler for nofound ones

---
+++ src/main/java/lk/icbt/dentalclinic/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package lk.icbt.dentalclinic.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
